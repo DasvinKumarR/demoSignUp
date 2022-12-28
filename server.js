4224d278d006b216a343765f9c85a569db671aca
DasvinKumarR <dasvinkumar2404@gmail.com>
2022-12-28 12:13:02 +0530

    setup

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is up at port 3000");
+});
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+    const firstName = String(req.body.firstName);
+    const lastName = String(req.body.lastName);
+    const email = String(req.body.email);
+    var firstNameLen = firstName.length;
+    var lastNameLen = lastName.length;
+    if (firstNameLen <= 3) {
+        res.sendFile(__dirname + "/errorMsg.html");
+    }
+    else if (lastNameLen <= 3) {
+       res.sendFile(__dirname + "/errorMsg.html");
+    }
+    else {
+        res.sendFile(__dirname + "/success.html");
+    }
+});
+
+app.post("/errorMsg", function(req, res){
+    res.redirect("/");
+});
+
+
+
+
+
